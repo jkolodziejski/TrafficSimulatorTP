069dc64d81e8d3aa258ae45f3c0ef6334c246759
celina2944t <99668744+celina2944t@users.noreply.github.com>
2022-03-17 16:01:40 +0100

    Update MainWindow.java

--- src/simulator/view/MainWindow.java
+++ src/simulator/view/MainWindow.java
@@ -11,6 +11,8 @@ import simulator.model.TrafficSimObserver;
 
 public class MainWindow extends JFrame implements TrafficSimObserver {
 	
+	private static final long serialVersionUID = 1L;		// only so the warning for MainWindow disappeared 
+	
 	private Controller _ctrl;
 
 	public MainWindow(Controller ctrl) {
@@ -23,28 +25,35 @@ public class MainWindow extends JFrame implements TrafficSimObserver {
 	private void initGUI() {
 		JPanel mainPanel = new JPanel(new BorderLayout());
 		this.setContentPane(mainPanel);
+		
 		mainPanel.add(new ControlPanel(_ctrl), BorderLayout.PAGE_START);
 		mainPanel.add(new StatusBar(_ctrl), BorderLayout.PAGE_END);
+		
 		JPanel viewsPanel = new JPanel(new GridLayout(1, 2));
 		mainPanel.add(viewsPanel, BorderLayout.CENTER);
+		
 		JPanel tablesPanel = new JPanel();
 		tablesPanel.setLayout(new BoxLayout(tablesPanel, BoxLayout.Y_AXIS));
 		viewsPanel.add(tablesPanel);
+		
 		JPanel mapsPanel = new JPanel();
 		mapsPanel.setLayout(new BoxLayout(mapsPanel, BoxLayout.Y_AXIS));
 		viewsPanel.add(mapsPanel);
+		
 		// tables
 		JPanel eventsView = createViewPanel(new JTable(new EventsTableModel(_ctrl)), "Events");
 		eventsView.setPreferredSize(new Dimension(500, 200));
 		tablesPanel.add(eventsView);
 		// TODO add other tables
 		// ...
+		
 		// maps
 		JPanel mapView = createViewPanel(new MapComponent(_ctrl), "Map");
 		mapView.setPreferredSize(new Dimension(500, 400));
 		mapsPanel.add(mapView);
 		// TODO add a map for MapByRoadComponent
 		// ...
+		
 		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 		this.pack();
 		this.setVisible(true);
